03bcf7429045a7dd196b5776b876051e8ef65f82
Jay Patel <jcp313@gmail.com>
2016-10-31 15:43:09 -0400

    added testing conditions

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,11 +8,46 @@ namespace UnitTestProject1
     {
 
 
-        [TestMethod]
+        [TestMethod]//attribute to run as a test
+        public void TestMethod0()
+        {
+            int index = lab6_real.PigLatinProgram.FirstVowelMethod("exit");
+            //int index made for testing, namespace.class.method(input variable)
+            Assert.AreEqual(0,index);//index should = 0
+        }
+
+        [TestMethod]//attribute to run as a test
         public void TestMethod1()
         {
-
-
+            int index = lab6_real.PigLatinProgram.FirstVowelMethod("corner");
+            //int index made for testing, namespace.class.method(input variable)
+            Assert.AreEqual(1, index);//index should = 0
         }
+
+        [TestMethod]//attribute to run as a test
+        public void TestMethodT1()
+        {
+            int index = lab6_real.PigLatinProgram.TranslateMethod("189");
+            //int index made for testing, namespace.class.method(input variable)
+            Assert.AreEqual(189, index);//index should = 0
+        }
+
+        [TestMethod]//attribute to run as a test
+        public void TestMethodT2()
+        {
+            int index = lab6_real.PigLatinProgram.TranslateMethod("hello@grandcircus.co");
+            //int index made for testing, namespace.class.method(input variable)
+            Assert.AreEqual("hello@grandcircus.co", index);//index should = 0
+        }
+
+        [TestMethod]//attribute to run as a test
+        public void TestMethodT3()
+        {
+            int index = lab6_real.PigLatinProgram.TranslateMethod("can't");
+            //int index made for testing, namespace.class.method(input variable)
+            Assert.AreEqual("can't", index);//index should = 0
+        }
+
+
     }
 }
