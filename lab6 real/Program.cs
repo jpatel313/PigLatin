c21c9c12ebac61d46b9d782373b84b8f27e562a7
Jay Patel <jcp313@gmail.com>
2016-10-31 14:12:01 -0400

    initial commit

---
+++ lab6 real/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_real
+{
+    public class PigLatinProgram
+    {
+        string vowelOutput = null;
+       // string notVowel = null;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter one word: ");
+            string userInput = Console.ReadLine();//USER INPUT
+            userInput = userInput.ToLower();//USER INPUT to all lowercase
+            string vowels = ("aeiou");
+
+
+
+            return StartsWithVowelMethod(vowelOutPut);
+        }
+
+        private static string StartsWithVowelMethod(string userInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartsWithVowelMethod(string userInput, string vowelOutPut)
+        {
+            //string of vowels
+            string vowelStart = null;
+            string consonantStart = null;
+            string notVowel = userInput.Contains(vowels);
+           // char[] vowelsCharArray = vowels.ToCharArray();//char array vowels
+
+            if (userInput.Contains(vowelsCharArray[0]))
+            {
+                vowelOutput = (userInput + "way");
+                return vowelOutput;
+            }
+            else if (Indexof[1])
+            //userInput.Contains(vowelsCharArray[1]))
+            {
+                
+
+            }
+
+            public void StartsWithConsonantMethod(string userInput)
+                {
+            }
+
+           
+
+
+                    
+
+
+                {
+                    Console.WriteLine(userInput.TrimStart[0]);
+                }                
+
+                int userInput.IndexOf('a', 'e', 'i', 'o', 'u');
+
+                userInput.Substring();
+
+                userWord.Append(userInput1.ToLower + 
+
+               vowels. = ("a");
+               vowels = "b";
+
+                Array.ForEach
+                string test = "";
+                bool isVowelA = vowels.Contains("a");
+
+
+
+                if (string userInput == )
+                {
+
+                }
+
+                         /*Turn words that start with vowels directly into igLatinpay 
+                string vowelFirstLetter = userInput + way;
+                StringBuilder vowels = new StringBuilder("a");
+                vowels = 
+
+                string a = "a";
+                string e = "e";
+                string i = "i";
+                string o = "o";
+                string u = "u";
+                string vowelsString ="AEIOUaeiou"
+                if (userInput.StartsWith(a) || userInput.StartsWith(e))
+                */
+
+            //char[] userInputArray = userInput.ToCharArray();//turn userInput into char array
+
+
+
+        }            
+    }
+}
+
